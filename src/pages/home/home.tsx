6dd0844b2f9d822c98362c39257bcb74de89907f
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-24 08:48:41 -0300

    Adiconado as dependencias do Scss de forma correta

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,9 @@
-
+import "./home.scss"
 
 export default function Home(){
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className={ "teste" }><span>Home</span></h1>
         </div>
     )
 }
