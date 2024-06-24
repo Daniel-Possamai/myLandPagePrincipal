01182ef4c96f1d4d0e15880f5506fe4e73cdbfbd
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-24 15:08:20 -0300

    recomeçado header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,31 @@
-import "./header.scss"
+import React, { useEffect, useState } from 'react';
 
+export default function Header() {
+    const [isFixed, setIsFixed] = useState(false);
 
-export default function Header(){
+    useEffect(() => {
+        const handleScroll = () => {
+            const shouldBeFixed = window.scrollY > 150;
+            setIsFixed(shouldBeFixed);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const style: React.CSSProperties = { // Especifica o tipo aqui
+        opacity: 1,
+        position: isFixed ? 'fixed' : 'relative',
+        top: 0,
+        width: '100%',
+        transition: 'all 0.5s ease',
+        zIndex: 10,
+    };
 
     return (
-        <header>
-            <img className={ "logo" } src="./images/myLogoBig.png" alt="Logo do desenvolvedor em questão na página"/>
-        </header>
-    )
+            <header style={style}>
+                <img className="logo" src="./images/myLogoBig.png" alt="Logo" />
+            </header>
+    );
 }
