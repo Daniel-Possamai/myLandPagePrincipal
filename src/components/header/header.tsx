d38e6483b9e0136b84ce887352d4e071366f5bc3
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-25 09:18:33 -0300

    Selecionado imagem de logo do nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,31 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import './header.scss'
 
 export default function Header() {
-    const [isFixed, setIsFixed] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            const shouldBeFixed = window.scrollY > 150;
-            setIsFixed(shouldBeFixed);
-        };
-
-        window.addEventListener('scroll', handleScroll);
-
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-    const style: React.CSSProperties = { // Especifica o tipo aqui
-        opacity: 1,
-        position: isFixed ? 'fixed' : 'relative',
-        top: 0,
-        width: '100%',
-        transition: 'all 0.5s ease',
-        zIndex: 10,
-    };
-
     return (
-            <header style={style}>
-                <img className="logo" src="./images/myLogoBig.png" alt="Logo" />
+            <header>
+                <img className="logo" src="./images/logoDesktopFinal.png" alt="Logo" />
             </header>
     );
 }
