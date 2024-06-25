2d37b4eef14b4dc9dda5179ae6f9d45c112b600a
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-25 20:24:45 -0300

    feita animação do hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,9 +3,19 @@ import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
 
 export default function Hero() {
     return (
-        <div className={ 'container-hero' }>
-            <p>Landing Page</p>
-            <ExpandMoreRoundedIcon className={ 'arrow-page' }/>
+        <div className={'container-hero'}>
+            <div className={ "animated-title" }>
+                <div className= {"text-top"} >
+                    <div>
+                        <span>Adquira</span>
+                        <span>sua Landing Page</span>
+                    </div>
+                </div>
+                <div className= { "text-bottom" }>
+                    <div>e expanda a sua presença na internet!</div>
+                </div>
+            </div>
+            <ExpandMoreRoundedIcon className={'arrow-page'} />
         </div>
     )
 }
