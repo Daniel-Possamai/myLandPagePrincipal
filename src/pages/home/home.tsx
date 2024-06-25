1ec1855a5759a133a2208c88260382d779914c09
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-25 11:45:58 -0300

    atualizado logo e animações

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,7 @@ import "./home.scss"
 export default function Home(){
     return (
         <div>
-            {/* <PreLoader/> */}
+            <PreLoader/>
 
             <a href="#"> <Header/> </a>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, dicta perferendis, cum ullam suscipit ut exercitationem repudiandae pariatur aliquam quis dolore, quas adipisci. Officiis magni natus eaque harum unde at.</div>
