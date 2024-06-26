b1aecae854cdfabcd2ea6828bb41bf1acc4f28e9
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-25 21:38:41 -0300

    atualizado responsivo da flecha

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,17 +2,36 @@ import Logo from "../../components/logo/logo"
 import Hero from "../../components/hero/hero"
 import PreLoader from "../../components/preloader/preloader"
 import "./home.scss"
+import { useEffect, useState } from "react"
+
+
+
+
 
 export default function Home(){
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+    }, [])
+
+
     return (
         <div className={ 'container-home' }>
-            {/* <PreLoader/> */}
 
-            <div className={ 'container-Englobador' }>
+            { loading ?
+             ( <PreLoader/> ) 
+             : 
+             <div className={ 'container-Englobador' }>
                 <Hero/>
                 <div className="teste">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eius laboriosam officia vitae illum! Omnis dolore voluptatem eveniet similique debitis nisi quod iure, aliquid culpa vitae eos cum? Sit, voluptatibus?</div>
             </div>
+ }
+            
 
+            
             
         </div>
     )
