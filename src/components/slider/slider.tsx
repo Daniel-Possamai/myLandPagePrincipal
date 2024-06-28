4106cc97b9f2aa43e247b1ce479bcc35f712d673
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-28 10:48:49 -0300

    estrutura basica do slider feita

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -8,7 +8,7 @@ export default function Slider() {
     <div className="container-g-slider">
       <div className="container-m-slider">
         <div className="image-slider">
-          <img src="#" alt="" />  
+          <img src="./images/exclusive.png" alt="" />  
         </div>
         <div className="infos-slider">
           <h2>Titulo do slide</h2>
@@ -16,6 +16,7 @@ export default function Slider() {
           <button>Botão</button>
         </div>
         <div className="nav-slider">
+          <button>1</button>
           {/* botoes conforme a quantidade de slides */}
         </div>
       </div>
