6ead7c2d1dcdcc75bcc2af21ba51b3270dc691b6
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-02 20:53:24 -0300

    att

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -37,9 +37,7 @@ export default function Home(){
              : 
              <div className={ 'container-Englobador' }>
                 <Hero/>
-                {/* <div className="teste">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eius laboriosam officia vitae illum! Omnis dolore voluptatem eveniet similique debitis nisi quod iure, aliquid culpa vitae eos cum? Sit, voluptatibus?</div> */}
                 <Objectpage/>
-                {/* <div className="teste">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eius laboriosam officia vitae illum! Omnis dolore voluptatem eveniet similique debitis nisi quod iure, aliquid culpa vitae eos cum? Sit, voluptatibus?</div> */}
                 <Slider slides={ SliderData }/>
             </div>
         }
