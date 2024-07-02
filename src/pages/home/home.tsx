8c8a1aedde3a4641ac4a09b8db08ef5b2ee55b1a
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-02 20:44:02 -0300

    atualizado responsividade do slider

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -32,17 +32,17 @@ export default function Home(){
     return (
         <div className={ 'container-home' }>
 
-            {/* { loading ?
+            { loading ?
              ( <PreLoader/> ) 
              : 
              <div className={ 'container-Englobador' }>
-                <Hero/> */}
+                <Hero/>
                 {/* <div className="teste">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eius laboriosam officia vitae illum! Omnis dolore voluptatem eveniet similique debitis nisi quod iure, aliquid culpa vitae eos cum? Sit, voluptatibus?</div> */}
                 <Objectpage/>
                 {/* <div className="teste">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem eius laboriosam officia vitae illum! Omnis dolore voluptatem eveniet similique debitis nisi quod iure, aliquid culpa vitae eos cum? Sit, voluptatibus?</div> */}
                 <Slider slides={ SliderData }/>
-            {/* </div>
-        } */}
+            </div>
+        }
             
 
             
