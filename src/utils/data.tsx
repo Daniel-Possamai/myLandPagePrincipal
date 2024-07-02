c1ff8e6309a9d396191a6c433a4ebc72a49715e3
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-02 13:53:15 -0300

    atualizado o slider para ficar mais suavemente a pagina

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -5,24 +5,24 @@ export const SliderData: SliderProps[] = [
         title: "Clínica de Fisioterapia",
         description: `Site simples e responsivo para apresentar a clínica. Destaca serviços, equipe e contato.`,
         url: "https://pronares.com.br/",
-        img: "./images/exclusive.png"
+        img: "./images/pronarespreview.png"
     },
     {
         title: "Imobiliária",
         description: "Site moderno para mostrar locações e imóveis à venda. Recursos como busca avançada e galeria de fotos.",
         url: "https://daniel-possamai.github.io/imobiliariaXYZ/",
-        img: "./images/exclusive.png"
+        img: "./images/imovelpreview.png"
     },
     {
         title: "Meu Portfólio",
         description: "Destaca habilidades, projetos e informações de contato.",
         url: "https://daniel-possamai.github.io/portfolioDeDanielPossamai/",
-        img: "./images/logoBg.png"
+        img: "./images/previewportfolio.png"
     },
     {
         title: "Acumulador de Links",
         description: "Site para salvar e categorizar links importantes.",
         url: "https://daniel-possamai.github.io/projetoDiscover/",
-        img: "./images/exclusive.png"
+        img: "./images/linkspreview.png"
     }
 ]
