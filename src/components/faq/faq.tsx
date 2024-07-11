5b92593b1a11375e4f007e92f6999d7733707171
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-11 08:58:06 -0300

    Atualizado o conteudo do FAQ

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -7,27 +7,21 @@ const data = {
     title: "FAQ (perguntas frequentes)",
     rows: [
         {
-            title: "Lorem ipsum dolor sit amet,",
-            content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed tempor sem. Aenean vel turpis feugiat,
-              ultricies metus at, consequat velit. Curabitur est nibh, varius in tellus nec, mattis pulvinar metus.
-              In maximus cursus lorem, nec laoreet velit eleifend vel. Ut aliquet mauris tortor, sed egestas libero interdum vitae.
-              Fusce sed commodo purus, at tempus turpis.`,
+            title: "O que é uma Landing Page?",
+            content: `Uma Landing Page é uma página da web criada com o objetivo específico de converter visitantes em leads ou clientes. Ela geralmente contém informações concisas, um formulário de captura de dados (como e-mail) e uma chamada para ação (CTA). As Landing Pages são usadas para promover produtos, serviços ou infoprodutos com alta conversão`,
         },
         {
-            title: "Nunc maximus, magna at ultricies elementum",
+            title: "Por que criar uma Landing Page?",
             content:
-                "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam, vitae convallis ex tortor sed dolor.",
+                "Uma landing page bem projetada pode aumentar a taxa de conversão de visitantes em clientes ou leads. Dessa forma, a empresa maximiza o retorno sobre o investimento em marketing. É possível usar e segmentar esses leads e planejar campanhas futuras de acordo com cada perfil de cliente.",
         },
         {
-            title: "Curabitur laoreet, mauris vel blandit fringilla",
-            content: `Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc, ac sagittis leo elit vel lorem.
-            Fusce tempor lacus ut libero posuere viverra. Nunc velit dolor, tincidunt at varius vel, laoreet vel quam.
-            Sed dolor urna, lobortis in arcu auctor, tincidunt mattis ante. Vivamus venenatis ultricies nibh in volutpat.
-            Cras eu metus quis leo vestibulum feugiat nec sagittis lacus.Mauris vulputate arcu sed massa euismod dignissim. `,
+            title: "O que é um design responsivo?",
+            content: `Um site responsivo se adapta automaticamente ao tamanho da tela do dispositivo que você está usando, seja um computador, tablet ou smartphone. Isso garante que a experiência de navegação seja sempre confortável e legível, independentemente do dispositivo.`,
         },
         {
-            title: "What is the package version",
-            content: <p>current version is 1.2.1</p>,
+            title: "Entenda o que é SEO e sua importância para o site",
+            content: `SEO (Search Engine Optimization) é o conjunto de estratégias e técnicas utilizadas para melhorar a visibilidade e o posicionamento de um site nos resultados orgânicos dos mecanismos de busca, como o Google, Bing e Yahoo. O objetivo do SEO é aumentar o tráfego de visitantes qualificados ao site, proporcionando maior exposição e, consequentemente, potencializando as oportunidades de conversão e vendas. Basicamente, o SEO busca otimizar o design e o conteúdo do site para que ele apareça nos primeiros resultados dos motores de busca, tornando-o mais visível e atraindo visitantes de forma orgânica`,
         },
     ],
 };
