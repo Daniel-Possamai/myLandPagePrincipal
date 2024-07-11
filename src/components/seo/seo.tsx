c529426160a6f1f5fdeac0dd6475415f9d877788
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-11 11:30:51 -0300

    Inciaido a estrutura do componente SEO

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -1,14 +1,28 @@
 import './seo.scss'
 
-export default function Seo(){
+export default function Seo() {
     return (
         <div className="container-g-seo">
-            <div className="infos-seo">
-                <h1>SEO (Search Engine Optimization)</h1>
-                <p>Na WebSwift, estamos comprometidos em oferecer soluções digitais de alta qualidade para nossos clientes. Uma das maneiras mais eficazes de alcançar visibilidade online e superar a concorrência é através da otimização de mecanismos de busca (SEO).</p>
+            <div className="container-m-seo">
+                <div className="infos-seo">
+                    <h1>SEO (Search Engine Optimization)</h1>
+                    <p>Na WebSwift, estamos comprometidos em oferecer soluções digitais de alta qualidade para nossos clientes. Uma das maneiras mais eficazes de alcançar visibilidade online e superar a concorrência é através da otimização de mecanismos de busca (SEO).</p>
+                </div>
+                <div className="img-seo">
+                    <img src="./images/seoanalytics.png" alt="imagem do ilustrativa do SEO" />
+                </div>
             </div>
-            <div className="img-seo">
-                <img src="./" alt="" />
+
+            <hr />
+
+            <div className="container-m-seo">
+                <div className="img-seo">
+                    <img src="./images/seoanalyticstwo.png" alt="" />
+                </div>
+                <div className="infos-seo">
+                    <h1>SEO (Search Engine Optimization)</h1>
+                    <p>Na WebSwift, estamos comprometidos em oferecer soluções digitais de alta qualidade para nossos clientes. Uma das maneiras mais eficazes de alcançar visibilidade online e superar a concorrência é através da otimização de mecanismos de busca (SEO).</p>
+                </div>
             </div>
         </div>
     )
