6ec0859d14cf86dc58d0df6bc67fb4837c4a48cf
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-11 10:01:19 -0300

    Criado o componente seo e adicionado a imagem que vai ser usada no seo

---
+++ src/components/seo/seo.tsx
@@ -0,0 +1,14 @@
+import './seo.scss'
+
+export default function Seo(){
+    return (
+        <div className="container-g-seo">
+            <div className="infos-seo">
+
+            </div>
+            <div className="img-seo">
+                <img src="./" alt="" />
+            </div>
+        </div>
+    )
+}
