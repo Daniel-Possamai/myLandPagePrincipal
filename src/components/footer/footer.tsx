c31e080526b1e3c04994de946ef7ea7b1a9a8320
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-17 14:19:09 -0300

    Comentado conteudo do footer que nao vai ser mais utilizado

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,19 +1,19 @@
 import "./footer.scss"
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import XIcon from '@mui/icons-material/X';
+// import InstagramIcon from '@mui/icons-material/Instagram';
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import XIcon from '@mui/icons-material/X';
 
 
 export default function Footer(){
     return (
         <footer className={ 'footer' }>
-            <div className="socialLinks">
+            {/* <div className="socialLinks">
                 <ul>
                     <li><a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FacebookIcon/></a></li>
                     <li><a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><InstagramIcon/></a></li>
                     <li><a href="https://www.twitter.com/" target="_blank" rel="noreferrer"><XIcon/></a></li>
                 </ul>
-            </div>
+            </div> */}
             <div className="line-down">
                 <p>Copyright © 2024 Bruordi e Legend. Todos os direitos reservados.</p>
             </div>
