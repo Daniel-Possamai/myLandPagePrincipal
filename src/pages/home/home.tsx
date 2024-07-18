dcb4935711f67a83942f51d4bf673fd46907ceb2
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-18 10:45:00 -0300

    Atualizado a cor do preloader

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -57,8 +57,8 @@ export default function Home(){
                 <Faaq/>
             
             </div>
-        {/* } */}
-            
+        {/* } 
+             */}
         {loading ? ""
             :
             <Footer/>}
