f3bc69905696fa5c764eeb1b0b6691f9694662cf
Daniel Possamai <daniel.possamai20@gmail.com>
2024-08-05 10:46:53 -0300

    Atualizada nomes de classes e ids para não dar conflito

--- src/pages/socials/social.tsx
+++ src/pages/socials/social.tsx
@@ -6,7 +6,7 @@ import './socials.scss'
 export default function Socials(){
     return (
         <div className="container-socials">
-             <div id="container">
+             <div id="container-social">
         <div id="profile">
             <img src="./images/logowebswift.png" alt="imagem de perfil da logo da webswift" />
             <p>@webswift</p>
@@ -26,11 +26,11 @@ export default function Socials(){
                 <a href="https://spotify.link/GKor3U167Jb" target="_blank">Playlist para treinar </a>
             </li>
         </ul>
-        <div id="socialLinks">
+        <div id="socialLinks-socials">
             <a href="https://www.instagram.com/webswift/" target="_blank">
             </a>
         </div>
-        <footer>
+        <footer className='footer-socials'>
             <a href="https://wa.me/49998241133" target="_blank">Desenvolvido por <span>WebSwift</span></a>
         </footer>
     </div>
