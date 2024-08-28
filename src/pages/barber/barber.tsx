041f830903564a66305ad88521145b851f840ab1
Daniel Possamai <daniel.possamai20@gmail.com>
2024-08-28 09:11:37 -0300

    Criada logica para senha na pagina de barbearia

--- src/pages/barber/barber.tsx
+++ src/pages/barber/barber.tsx
@@ -1,10 +1,43 @@
+import React, { useEffect, useState } from "react";
 import "./barber.scss";
 
 
 
 export default function Barber(){
+
+    const [valid, setValid] = useState(false)
+    const [senha, setSenha] = useState('')
+
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSenha(event.target.value)
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if(senha === '2023'){
+            setValid(true)
+        }
+    }
+
     return(
-        <div>test</div>
+        <div>
+            {valid 
+            ? 
+            <h1>Barbeiro</h1>
+            :  
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="senha">
+                    Senha:
+                    <input name="senha" type="number" value={senha} onChange={handleChange}/>
+                </label>
+                <button type="submit">Acessar</button>
+            </form> }
+            
+
+
+        </div>
+        
 
     )
 }
